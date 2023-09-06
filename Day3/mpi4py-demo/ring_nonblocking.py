e2e05f4697319462ae7ba16ea0e06af250cc0e08
Silva Alejandro Ismael <silva.alejandro.ismael@gmail.com>
2023-09-06 16:06:23 -0500

    day 3: mpi4py

---
+++ Day3/mpi4py-demo/ring_nonblocking.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+me = comm.Get_rank()
+ncpu = comm.Get_size()
+
+# set rank of previous and next process
+prev = me - 1
+if prev < 0:
+    prev = ncpu - 1
+
+next = me + 1
+if next == ncpu:
+    next = 0
+
+# at the beginning rank 0 will hold the message
+mesg = me
+# if me == 0:
+#     mesg = 1
+
+for i in range(0,20):
+    # post non-blocking receives first, then send, then wait for request to complete
+    req = comm.irecv()
+    comm.send(mesg, next)
+    mesg = req.wait()
+    # communication complete.
+    # if mesg:
+    #     print("step %d: rank %d has message" % (i, me))
+
+    list = comm.gather(mesg)
+    
+    if me == 0:
+        print(f"step {i:02} messages: {list}")
