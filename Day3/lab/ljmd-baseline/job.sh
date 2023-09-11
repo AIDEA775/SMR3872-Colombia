d72253db6929147a16e0b4e9daa1ad25bad1bb46
Silva Alejandro Ismael <silva.alejandro.ismael@gmail.com>
2023-09-11 16:54:22 -0500

    day 6: ljmd: openmp + mpi. add job.sh

---
+++ Day3/lab/ljmd-baseline/job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -A ICT23_SMR3872
+#SBATCH -p boost_usr_prod
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=32
+#SBATCH --time 00:10:00
+#SBATCH --gres=gpu:0
+#SBATCH --mem=490000MB
+
+#modulefiles to be loaded to have MPI on Leonardo
+module purge
+module load openmpi/4.1.4--gcc--11.3.0-cuda-11.8
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+make check impl=both size=2916
