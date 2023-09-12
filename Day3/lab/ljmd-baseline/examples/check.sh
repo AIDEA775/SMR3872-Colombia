5b6930cec1ac6fbaa59ee61786798c5cfc70db43
Silva Alejandro Ismael <silva.alejandro.ismael@gmail.com>
2023-09-11 23:34:26 -0500

    day 6: ljmd: openmp + mpi. alternative check.sh

--- Day3/lab/ljmd-baseline/examples/check.sh
+++ Day3/lab/ljmd-baseline/examples/check.sh
@@ -1,4 +1,8 @@
-mpirun ../ljmd-${impl}.x < argon_${size}.inp
+size=2916
+
+srun -A ICT23_SMR3872 -p boost_usr_prod --nodes=3 --ntasks-per-node=32 --cpus-per-task=1 --mem=490000MB \
+    ../ljmd-mpi.x < argon_${size}.inp
+
 head -10 argon_${size}.dat | awk '{printf("%d %.6f %.6f %.6f\n",$1,$2,$3,$4);}' > a.dat
 head -10 ../reference/argon_${size}.dat | awk '{printf("%d %.6f %.6f %.6f\n",$1,$2,$3,$4);}' > b.dat
 cmp a.dat b.dat || exit 1
